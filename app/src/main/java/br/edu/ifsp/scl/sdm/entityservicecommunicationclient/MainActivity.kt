f51922125c5660468921a16a8b0fcd9e61734097
RafaelCostaFreitas <freitas.rafael1@aluno.ifsp.edu.br>
2026-03-09 21:48:36 -0300

    parte4 - final

--- app/src/main/java/br/edu/ifsp/scl/sdm/entityservicecommunicationclient/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/entityservicecommunicationclient/MainActivity.kt
@@ -10,6 +10,7 @@ import android.os.Looper
 import android.os.Message
 import android.os.Messenger
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import br.edu.ifsp.scl.sdm.entityservicecommunicationclient.databinding.ActivityMainBinding
 
@@ -32,30 +33,55 @@ class MainActivity : AppCompatActivity() {
                        override fun handleMessage(msg: Message) {
                            super.handleMessage(msg)
                            counter = msg.data.getInt("VALUE")
-                           TOAS
+                           Toast.makeText(this@MainActivity, "Você clicou $counter vezes", Toast.LENGTH_LONG).show()
 
                        }
-                   })
+                   }).also { messenger ->
+                       replyTo = messenger }
                })
            }
         }
 
         override fun onServiceDisconnected(name: ComponentName?) {
-            TODO("Not yet implemented")
+            Log.v(getString(R.string.app_name), "cliente esta desconectado desse serviço.")
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
+
+        incrementBoundServiceIntent = Intent().apply {
+            component =  ComponentName(
+                "br.edu.ifsp.scl.sdm.entityservicecommunication",
+                "br.edu.ifsp.scl.sdm.entityservicecommunication.IncrementBoundService"
+            )
+        }
+        if(!bindService(
+                incrementBoundServiceIntent,
+            incrementBoundServiceConnection,
+                BIND_AUTO_CREATE //vinculando ao serviço que foi criado 36:00min
+        )){
+            Toast.makeText(this, "Não foi possível vincular ao serviço", Toast.LENGTH_LONG).show()
+            finish()
+        }
+
         with(amb) {
             mainTb.apply {
                 getString(R.string.app_name).also { setTitle(it) }
                 setSupportActionBar(this)
             }
             incrementBt.setOnClickListener {
+                ibsMessenger.send(Message.obtain().apply {
+                    data.putInt("VALUE", counter)
+                })
 
             }
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unbindService(incrementBoundServiceConnection)
     }
 }
